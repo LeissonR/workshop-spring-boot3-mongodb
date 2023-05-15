524ae820c27834e17d1c8a93c6f4ba39c5033cfa
Leisson Rodrigues <leisson@gmail.com>
2023-05-15 19:10:01 -0400

    Insert created

--- src/main/java/com/leisson/workshopmongo/services/UserService.java
+++ src/main/java/com/leisson/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leisson.workshopmongo.domain.User;
+import com.leisson.workshopmongo.dto.UserDTO;
 import com.leisson.workshopmongo.repository.UserRepository;
 import com.leisson.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);	
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
 
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
 }
