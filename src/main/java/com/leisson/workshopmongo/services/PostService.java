728175f5e03190c9f7312ca036f41e6ef0645490
Leisson Rodrigues <leisson@gmail.com>
2023-05-16 13:11:23 -0400

    Getting a post by id

---
+++ src/main/java/com/leisson/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.leisson.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leisson.workshopmongo.domain.Post;
+import com.leisson.workshopmongo.repository.PostRepository;
+import com.leisson.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);	
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
+
