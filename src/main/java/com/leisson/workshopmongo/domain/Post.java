581c09b8b57b282dc1ee1fa6068d3d22544b8bb9
Leisson Rodrigues <leisson@gmail.com>
2023-05-16 14:23:15 -0400

    Adding comments to posts

--- src/main/java/com/leisson/workshopmongo/domain/Post.java
+++ src/main/java/com/leisson/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.leisson.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.leisson.workshopmongo.dto.AuthorDTO;
+import com.leisson.workshopmongo.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable{
@@ -19,7 +22,9 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
-
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {
 	}
 
@@ -71,6 +76,10 @@ public class Post implements Serializable{
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -87,5 +96,5 @@ public class Post implements Serializable{
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
